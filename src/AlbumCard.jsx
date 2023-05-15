b5591a6fa0be2f256acee96d2ddaac5bc21388cd
Yas Rossan <yaaseen.rossan95@gmail.com>
2023-05-16 00:36:51 +0100

    updated album images

--- src/AlbumCard.jsx
+++ src/AlbumCard.jsx
@@ -10,7 +10,7 @@ export default function AlbumCard () {
             {Album.map(album => {
                 return (
                     <div className="AlbumContainer">
-                        <img src="https://upload.wikimedia.org/wikipedia/en/1/1a/Identity_%28Far_East_Movement_album%29.jpg"></img>
+                        <img src={album.Icon}></img>
                         <p>{album.Album}</p>
                         <p>{album.Genre}</p>
                         <p>{album.Recorded}</p>
